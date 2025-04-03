80bf9a22176cd01f30519acf2e9340286a63ecc0
Tewan0 <s6652410012@sau.ac.th>
2025-04-03 15:55:37 +0700

    3

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 exports.uploadUser = multer({
      storage: storage,
      limits: {
-         fileSize: 1000000
+         fileSize: 10000000
      },
      fileFilter: (req, file, cb) => {
          const fileTypes = /jpeg|jpg|png/;
